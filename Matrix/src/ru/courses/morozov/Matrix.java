5f52fbc97a81a7248145a32915a70ff2431c8f75
Veyron154 <De1ete54@mail.ru>
2015-10-29 13:02:44 +0600

    добавление методов subtraction, addition

--- Matrix/src/ru/courses/morozov/Matrix.java
+++ Matrix/src/ru/courses/morozov/Matrix.java
@@ -157,4 +157,24 @@ public class Matrix {
         }
         return auxiliaryMatrix;
     }
+
+    public static Matrix subtraction(Matrix matrix1, Matrix matrix2) {
+        if (matrix1.getCountOfStrings() != matrix2.getCountOfStrings() ||
+                matrix1.getCountOfColumns() != matrix2.getCountOfColumns()) {
+            throw new IllegalArgumentException("Вычитание возможно только при одинаковых размерах матриц");
+        }
+        Matrix auxiliaryMatrix = new Matrix(matrix1.getCountOfStrings(), matrix1.getCountOfColumns());
+        for (int i = 0; i < matrix1.getCountOfStrings(); ++i) {
+            for (int j = 0; j < matrix1.getCountOfColumns(); ++j) {
+                auxiliaryMatrix.matrixComponents[i][j] = matrix1.matrixComponents[i][j]
+                        - matrix2.matrixComponents[i][j];
+            }
+        }
+        return auxiliaryMatrix;
+    }
+
+    public Matrix addition(Matrix inputMatrix) {
+        this.matrixComponents = Matrix.addition(this, inputMatrix).matrixComponents;
+        return this;
+    }
 }
