5685a811a1cb164a40aceb08bb009590a40bd2c0
Veyron154 <De1ete54@mail.ru>
2015-11-01 15:37:25 +0600

    добавлены примеры в main

--- Matrix/src/ru/courses/morozov/Main.java
+++ Matrix/src/ru/courses/morozov/Main.java
@@ -5,10 +5,24 @@ package ru.courses.morozov;
  */
 public class Main {
     public static void main(String[] args) {
-        double[][] array = {{1, 2, 3}, {4, 5, 6}};
-        Matrix matrix = new Matrix(array);
+        double[][] array1 = {{4, 2, 7},{9, 0, 8},{8, 10, 2}};
+        double[][] array2 = {{3, 1, 6},{-3, 4, 12}};
+        double[][] array3 = {{4, 2, 7},{9, 0, 8},{8, 10, 2}};
+        double[] vector = {1, 2};
+        Matrix matrix1 = new Matrix(array1);
+        Matrix matrix2 = new Matrix(array2);
+        Matrix matrix3 = new Matrix(array3);
+        Vector vector1 = new Vector(2, vector);
 
-        System.out.println(matrix.toString());
+        System.out.println(matrix1.getColumn(1));
+        System.out.println(matrix2.getString(0));
+        System.out.println(matrix1.getDeterminant());
+        System.out.println(Matrix.subtraction(matrix1, matrix2));
+        System.out.println(matrix1.equals(matrix3));
+        System.out.println(matrix1.hashCode() == matrix3.hashCode());
+        System.out.println(matrix2.transpose());
+        System.out.println(Matrix.multiply(matrix1, matrix2));
+        System.out.println(matrix2.multiplyByVector(vector1));
     }
 
 }
