9be5b4fb999b3eef7c7a6de639beb90533dc351c
Veyron154 <De1ete54@mail.ru>
2015-11-06 23:24:58 +0600

    добавлены методы getRang, setNonZeroComponent

--- Gauss/src/ru/courses/morozov/Gauss.java
+++ Gauss/src/ru/courses/morozov/Gauss.java
@@ -20,8 +20,7 @@ public class Gauss {
 
     private static Matrix directFlow(Matrix matrix) {
         for (int i = 0; i < matrix.getCountOfStrings(); ++i) {
-            matrix.setString(i, matrix.getString(i).multiplyByScalar
-                    (1 / matrix.getString(i).getVectorComponent(i)));
+            matrix.setString(i, matrix.getString(i).multiplyByScalar(1 / matrix.getString(i).getVectorComponent(i)));
             for (int j = i + 1; j < matrix.getCountOfStrings(); ++j) {
                 matrix.setString(j, matrix.getString(j).subtraction(matrix.getString(i).
                         multiplyByScalar(matrix.getString(j).getVectorComponent(i))));
@@ -41,13 +40,14 @@ public class Gauss {
     }
 
     public static void main(String[] args) {
-        double[][] array1 = {{3, 2, -5}, {2, -1, 3}, {1, 2, -1}};
+        double[][] array1 = {{0, 2, -5}, {0, -1, 3}, {0, 2, -1}, {4, 5, 9}};
         double[] array2 = {-1, 13, 9};
 
         Matrix matrix = new Matrix(array1);
         Vector vector = new Vector(3, array2);
 
-        System.out.println(gauss(matrix, vector));
+        //System.out.println(getRang(directFlow(matrix)));
+        System.out.println(setNonZeroComponent(matrix, 0, 0));
     }
 
     private static Vector gauss(Matrix matrix, Vector vector) {
@@ -55,5 +55,47 @@ public class Gauss {
         directFlow(operatingMatrix);
         reversal(operatingMatrix);
         return new Vector(operatingMatrix.getColumn(operatingMatrix.getCountOfColumns() - 1));
+    }
+
+    private static int getRang(Matrix matrix){
+        int rang = 0;
+        for(int i = 0; i < matrix.getCountOfStrings(); ++i){
+            if (matrix.getString(i).equals(new Vector(matrix.getCountOfColumns()))){
+                continue;
+            }
+            ++rang;
+        }
+        return rang;
+    }
+
+    public static Matrix setNonZeroComponent(Matrix matrix, int string, int column){
+        //проверка, что в заданном столбце есть не нулевой элемент.
+        for(int i = string; i < matrix.getCountOfStrings(); ++i){
+            if(matrix.getString(i).getVectorComponent(column) != 0){
+                //замена строки с нулевым компонентом.
+                for(int j = string; j < matrix.getCountOfStrings(); ++j){
+                    if(matrix.getString(j).getVectorComponent(column) == 0) {
+                        Vector auxiliaryVector = new Vector(matrix.getString(j));
+                        for (int k = j; k < matrix.getCountOfStrings() - 1; ++k) {
+                            matrix.setString(k, matrix.getString(k + 1));
+                        }
+                        matrix.setString(matrix.getCountOfStrings() - 1, auxiliaryVector);
+
+                        //проверка, что все элементы начиная с j-ого равны нулю. (чтобы выйти из бесконечного цикла)
+                        Vector zeroVector = new Vector(matrix.getCountOfStrings() - j);
+                        for(int l = j; l < matrix.getCountOfStrings(); ++l){
+                            zeroVector.setVectorComponent(l - j, matrix.getString(l).getVectorComponent(column));
+                        }
+                        if(zeroVector.equals(new Vector(zeroVector.getSize()))){
+                            break;
+                        }
+                        --j;
+                    }
+                }
+                break;
+            }
+        }
+        System.out.println(matrix);
+        return matrix;
     }
 }
