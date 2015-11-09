fc1f0f34f56b532a68f2ab483b67d4f6f12a40df
Veyron154 <De1ete54@mail.ru>
2015-11-09 18:47:53 +0600

    добавлен метод deleteObject

--- HashTable/src/ru/courses/morozov/HashTable.java
+++ HashTable/src/ru/courses/morozov/HashTable.java
@@ -4,23 +4,21 @@ import java.util.ArrayList;
 
 public class HashTable<T> {
     private ArrayList[] hashTable;
-    private T object;
-    private int arraySize = 128;
+    private int tableSize = 128;
 
     public HashTable() {
-        this.hashTable = new ArrayList[this.arraySize];
+        this.hashTable = new ArrayList[this.tableSize];
     }
 
     public HashTable(int arraySize) {
-        this.arraySize = arraySize;
+        this.tableSize = arraySize;
         this.hashTable = new ArrayList[arraySize];
     }
 
     public void setObject(T object) {
-        this.object = object;
-        int index = object.hashCode() % this.arraySize;
+        int index = object.hashCode() % this.tableSize;
         if (hashTable[index] == null) {
-            hashTable[index] = new ArrayList<T>();
+            hashTable[index] = new ArrayList();
         }
         hashTable[index].add(object);
     }
@@ -33,5 +31,10 @@ public class HashTable<T> {
             }
         }
         return countOfElements;
+    }
+
+    public void deleteObject(T object) {
+        int index = object.hashCode() % this.tableSize;
+        hashTable[index].remove(object);
     }
 }
