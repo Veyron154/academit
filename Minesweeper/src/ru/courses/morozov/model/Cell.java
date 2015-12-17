1df157ac6719c993f401fbea5f75397e77b567dd
Veyron154 <De1ete54@mail.ru>
2015-12-17 21:46:27 +0600

    создан класс Cell

---
+++ Minesweeper/src/ru/courses/morozov/model/Cell.java
@@ -0,0 +1,59 @@
+package ru.courses.morozov.model;
+
+public class Cell {
+    private int index;
+    private boolean flagged;
+    private boolean opened;
+    private boolean marked;
+    private int rowIndex;
+    private int columnIndex;
+
+    public Cell(int rowIndex, int columnIndex) {
+        this.rowIndex = rowIndex;
+        this.columnIndex = columnIndex;
+    }
+
+    public int getRowIndex() {
+        return this.rowIndex;
+    }
+
+    public int getColumnIndex() {
+        return this.columnIndex;
+    }
+
+    public int getIndex() {
+        return this.index;
+    }
+
+    public boolean isFlagged() {
+        return this.flagged;
+    }
+
+    public boolean isOpened() {
+        return this.opened;
+    }
+
+    public boolean isMarked() {
+        return this.marked;
+    }
+
+    public void upIndex() {
+        this.index += 1;
+    }
+
+    public void setIndex(int index) {
+        this.index = index;
+    }
+
+    public void setFlagged(boolean flagged) {
+        this.flagged = flagged;
+    }
+
+    public void setOpened(boolean opened) {
+        this.opened = opened;
+    }
+
+    public void setMarked(boolean marked) {
+        this.marked = marked;
+    }
+}
