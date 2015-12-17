6f8e7e7de2b9a9086bce980a81eb38f81d63d7b1
Veyron154 <De1ete54@mail.ru>
2015-12-17 21:49:33 +0600

    создан класс MineMain

--- Minesweeper/src/ru/courses/morozov/MineMain.java
+++ Minesweeper/src/ru/courses/morozov/MineMain.java
@@ -3,8 +3,10 @@ package ru.courses.morozov;
 
 import ru.courses.morozov.GUI.MineFrame;
 
+import java.io.IOException;
+
 public class MineMain {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException, ClassNotFoundException {
         MineFrame mineFrame = new MineFrame();
         mineFrame.run();
     }
