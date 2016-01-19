23eba3ab47ec75ad74eecf0e74299ee88a72d72f
Veyron154 <De1ete54@mail.ru>
2016-01-19 19:47:07 +0600

    сделано графическое отображение дерева

--- VisitTree/src/ru/courses/morozov/TreeMain.java
+++ VisitTree/src/ru/courses/morozov/TreeMain.java
@@ -1,5 +1,9 @@
 package ru.courses.morozov;
 
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class TreeMain {
     public static void main(String[] args) {
         TreeNode treeNode1 = new TreeNode(1);
@@ -23,23 +27,73 @@ public class TreeMain {
         treeNode4.addChild(treeNode9);
         treeNode7.addChild(treeNode10);
 
-        printTree(treeNode1);
-        System.out.println();
-        VisitWidth visitWidth = new VisitWidth();
-        visitWidth.visit(treeNode1);
-        //VisitDepth visitDepth = new VisitDepth();
-        //visitDepth.visit(treeNode1);
+        print(treeNode1);
+        //printTree(treeNode1);
+        //System.out.println();
+        //VisitWidth visitWidth = new VisitWidth();
+        //visitWidth.visit(treeNode1);
+        VisitDepth visitDepth = new VisitDepth();
+        visitDepth.visit(treeNode1);
     }
 
-    public static void printTree(TreeNode root){
+    public static void printTree(TreeNode root) {
         System.out.print("(");
         System.out.print(root.getIndexID());
-        for(int i = 0; i < root.getChildren().size(); ++i){
+        for (int i = 0; i < root.getChildren().size(); ++i) {
             printTree(root.getChildren().get(i));
         }
-        if(root.getChildren().size() == 0){
+        if (root.getChildren().size() == 0) {
             System.out.print("x");
         }
         System.out.print(")");
+    }
+
+    public static void print(TreeNode root) {
+        HashMap<TreeNode, Integer> mapOfNodes = new HashMap<>();
+        int rootWidth = countWidthOfNode(root, mapOfNodes);
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        int currentWidth = 0;
+        while (!queue.isEmpty()) {
+            TreeNode node = queue.remove();
+            int nodeWidth = mapOfNodes.get(node);
+            for (int i = 1; i <= (nodeWidth / 2); ++i) {
+                System.out.print(" ");
+            }
+            System.out.print(node.getIndexID());
+            for (int i = (nodeWidth / 2) + 1; i < nodeWidth; ++i) {
+                System.out.print(" ");
+            }
+            System.out.print(" ");
+            currentWidth = currentWidth + nodeWidth;
+            ++currentWidth;
+            if (currentWidth >= rootWidth) {
+                System.out.println();
+                currentWidth = 0;
+            }
+            queue.addAll(node.getChildren());
+        }
+        System.out.println();
+    }
+
+    public static int countWidthOfNode(TreeNode node, HashMap<TreeNode, Integer> map) {
+        int countOfChildren = node.getChildren().size();
+        if (countOfChildren == 0) {
+            if (node.getIndexID() < 10) {
+                map.put(node, 1);
+                return 1;
+            }
+            if (node.getIndexID() < 100) {
+                map.put(node, 2);
+                return 2;
+            }
+        }
+        int width = 0;
+        for (int i = 0; i < countOfChildren; ++i) {
+            width = width + countWidthOfNode(node.getChildren().get(i), map);
+        }
+        width = width + (countOfChildren - 1);
+        map.put(node, width);
+        return width;
     }
 }
