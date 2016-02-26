2d27babaad0ba951484eecd02cfe0894d639ac18
Veyron154 <De1ete54@mail.ru>
2016-02-26 18:15:34 +0600

    добавлена функция scanNumber

--- IntroJs/src/script.js
+++ IntroJs/src/script.js
@@ -2,43 +2,30 @@ var a;
 var b;
 var c;
 
-while (true) {
-    var inputA = +prompt("Введите число a", "");
-    if (isNaN(inputA)) {
-        alert("Введите число");
-    } else {
-        a = inputA;
-        break;
-    }
-}
-while (true) {
-    var inputB = +prompt("Введите число b", "");
-    if (isNaN(inputB)) {
-        alert("Введите число");
-    } else {
-        b = inputB;
-        break;
-    }
-}
-
-while (true) {
-    var inputC = +prompt("Введите число c", "");
-    if (isNaN(inputC)) {
-        alert("Введите число");
-    } else {
-        c = inputC;
-        break;
-    }
-}
+a = scanNumber("a");
+b = scanNumber("b");
+c = scanNumber("c");
 
 var d = Math.pow(b, 2) - 4 * a * c;
 
 if (d < 0) {
     console.log("Решений нет");
 } else if (d == 0) {
-    console.log("x1 = x2 = 0");
+    var x = -b / (2 * a);
+    console.log("x1 = x2 = " + x);
 } else {
     var x1 = (-b + Math.sqrt(d)) / (2 * a);
     var x2 = (-b - Math.sqrt(d)) / (2 * a);
     console.log("x1 = " + x1 + "\nx2 = " + x2)
+}
+
+function scanNumber(numberString) {
+    while (true) {
+        var tmpVar = +prompt("Введите число " + numberString, "");
+        if (isNaN(tmpVar)) {
+            alert("Введите число");
+        } else {
+            return tmpVar
+        }
+    }
 }
