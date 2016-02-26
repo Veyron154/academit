35aacd3e1a7d00a36b280fec6201fe5d9d5eda2a
Veyron154 <De1ete54@mail.ru>
2016-02-26 20:50:56 +0600

    добавлено решение задачи с чётными числами

--- IntroJs/src/script2.js
+++ IntroJs/src/script2.js
@@ -8,10 +8,16 @@ console.log(list);
 console.log(list.slice(0, 5));
 console.log(list.slice(list.length - 5));
 
-var list2 = [];
-
-for (var i = 0; i < 100; ++i) {
-    list2[i] = i + 1;
+var sum = 0;
+for (i = 0; i < list.length; ++i){
+    if (list[i] % 2 == 0){
+        sum += list[i];
+    }
 }
+console.log(sum);
 
+var list2 = [];
+for (var i = 1; i <= 100; ++i) {
+    list2.push(i);
+}
 console.log(list2);
