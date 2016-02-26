9fb0ff3267af86c274f4f8d36c0432e8894e3ae8
Veyron154 <De1ete54@mail.ru>
2016-02-26 18:30:43 +0600

    сделана задача со списками

--- IntroJs/src/script2.js
+++ IntroJs/src/script2.js
@@ -1,1 +1,17 @@
+var list = [6, 3, 7, 1, 0, 9, 8, 10, 2, 4, 5, 11];
 
+list.sort(function (e1, e2) {
+    return e2 - e1;
+});
+
+console.log(list);
+console.log(list.slice(0, 5));
+console.log(list.slice(list.length - 5));
+
+var list2 = [];
+
+for (var i = 0; i < 100; ++i) {
+    list2[i] = i + 1;
+}
+
+console.log(list2);
