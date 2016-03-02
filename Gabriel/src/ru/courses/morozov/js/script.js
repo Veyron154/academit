94459e8f6342bee098d4cf692c4917923d9fc576
Veyron154 <De1ete54@mail.ru>
2016-03-02 16:17:43 +0600

    изменены слайдеры

--- Gabriel/src/ru/courses/morozov/js/script.js
+++ Gabriel/src/ru/courses/morozov/js/script.js
@@ -1,29 +1,23 @@
 $(document).ready(function () {
     var sliders = $(".slider");
     var inputs = $(".main-table td:nth-child(6) input");
-    var maxValue = 1000;
+    var maxValue = 190;
     var minValue = 0;
-    var minStep = 10;
+    var step = 10;
+    var stepSwitchValue = 100;
+    var sliderCoefficient = 10;
 
     sliders.each(function (i) {
         $(this).slider({
             min: minValue,
             max: maxValue,
-            step: minStep,
-            slide: function () {
-                var sliderValue = sliders.eq(i).slider("value");
+            step: step,
+            slide: function (e, ui) {
+                var sliderValue = ui.value;
                 inputs.eq(i).val(sliderValue);
-                var stepSwitchValue = 100;
-                if (sliderValue >= stepSwitchValue) {
-                    var maxStep = 100;
-                    sliders.eq(i).slider("option", "step", maxStep);
-                }
-            },
-            stop: function () {
-                var sliderValue = sliders.eq(i).slider("value");
-                inputs.eq(i).val(sliderValue);
-                if (sliderValue === minValue) {
-                    sliders.eq(i).slider("option", "step", minStep);
+                if (sliderValue > stepSwitchValue) {
+                    var newValue = (ui.value - stepSwitchValue) * sliderCoefficient + stepSwitchValue;
+                    inputs.eq(i).val(newValue);
                 }
             }
         })
@@ -33,11 +27,17 @@ $(document).ready(function () {
         $(this).change(function () {
             var inputValue = inputs.eq(i).val();
             var valueCoefficient = 10;
+            var maxInputValue = 1000;
             if (inputValue % valueCoefficient !== 0) {
                 inputs.eq(i).val(inputValue - (inputValue % valueCoefficient));
             }
-            if (inputValue > maxValue) {
-                inputs.eq(i).val(maxValue);
+            if (inputValue > maxInputValue) {
+                inputs.eq(i).val(maxInputValue);
+            }
+            if (inputValue > stepSwitchValue) {
+                var newValue = (inputValue - stepSwitchValue) / sliderCoefficient + stepSwitchValue;
+                sliders.eq(i).slider("value", newValue);
+                return;
             }
             if (inputValue === "") {
                 inputs.eq(i).val("0");
