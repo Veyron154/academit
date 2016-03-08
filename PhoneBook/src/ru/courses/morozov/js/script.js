85b240fd7cb16e712c5a502bc301e89d148cd62f
Veyron154 <De1ete54@mail.ru>
2016-03-08 18:59:58 +0600

    создана страница PhoneBook

---
+++ PhoneBook/src/ru/courses/morozov/js/script.js
@@ -0,0 +1,149 @@
+$(document).ready(function () {
+    $(".submit").click(function (e) {
+        e.preventDefault();
+
+        var inputs = $(".input-phone-book");
+        var isCorrect = true;
+
+        inputs.each(function () {
+            if ($(this).val() == "") {
+                $(this).css("background-color", "pink");
+                isCorrect = false;
+            }
+        });
+
+        if (!isCorrect) {
+            $.alert({
+                title: "Ошибка заполнения",
+                content: "Заполните выделенные поля",
+                confirmButton: "OK"
+            });
+            return;
+        }
+
+        var surname = $("#surname");
+        var name = $("#name");
+        var phone = $("#phone");
+
+        var isUniquePhone = true;
+        var phones = $(".table-phone-book tr td:nth-child(4)");
+
+        phones.each(function () {
+            if ($(this).text() === phone.val()) {
+                isUniquePhone = false;
+            }
+        });
+
+        if (!isUniquePhone) {
+            $.alert({
+                title: "Ошибка заполнения",
+                content: "Контакт с номером " + phone.val() + " уже существует",
+                confirmButton: "OK"
+            });
+            return;
+        }
+
+        var tr = $("<tr></tr>");
+        $(".table-phone-book tbody").append(tr);
+        var tdIndex = $("<td></td>");
+        tr.append(tdIndex);
+        var tdSurname = $("<td></td>").text(surname.val());
+        tr.append(tdSurname);
+        var tdName = $("<td></td>").text(name.val());
+        tr.append(tdName);
+        var tdPhone = $("<td></td>").text(phone.val());
+        tr.append(tdPhone);
+        var deleteButton = $("<button>X</button>").click(function () {
+            var rows = $(".table-phone-book tr:has(td [type='checkbox']:checked)");
+            var messageString = "следующие контакты? :</br>";
+            $(".table-phone-book tr:has(td [type='checkbox']:checked) td:nth-child(2)").each(function () {
+                messageString += $(this).text() + "</br>";
+            });
+            if (rows.length === 0) {
+                rows = $(this).parents("tr");
+                messageString = "контакт: " + $(this).parents("tr").children("td:nth-child(2)").text() + " ?";
+            }
+
+            $.confirm({
+                title: "Подтверждение удаления",
+                content: "Вы действительно хотите удалить " + messageString,
+                confirmButton: "OK",
+                cancelButton: "Отмена",
+                confirm: function () {
+                    rows.remove();
+                    rowNumbered();
+                }
+            });
+        });
+        tr.append($("<td></td>").html(deleteButton));
+        var checkBox = $("<input />").attr("type", "checkbox");
+        tr.append($("<td></td>").html(checkBox));
+
+        rowNumbered();
+
+        inputs.each(function () {
+            $(this).css("background-color", "white").val("");
+        });
+    });
+
+    $(".top-checkbox").change(function () {
+        var checkboxes = $(".table-phone-book tr td:last-child [type='checkbox']");
+        if ($(this).prop("checked") == true) {
+            checkboxes.each(function () {
+                $(this).prop("checked", true);
+            })
+        } else {
+            checkboxes.each(function () {
+                $(this).prop("checked", false);
+            })
+        }
+    });
+
+    $(".button-filter-ok").click(function () {
+        var rows = $(".table-phone-book tbody tr");
+        rows.hide();
+        var tableData = $(".table-phone-book tbody tr td");
+        var filterText = $(".text-filter").val();
+        tableData.each(function () {
+            if ($(this).text().indexOf(filterText) !== -1) {
+                $(this).parents("tr").show();
+            }
+        });
+    });
+
+    $(".button-filter-clear").click(function () {
+        var rows = $(".table-phone-book tbody tr");
+        rows.show();
+        $(".text-filter").val("");
+    });
+});
+
+var rowNumbered = function () {
+    var rowsIndices = $(".table-phone-book tr td:first-child");
+    rowsIndices.each(function (i) {
+        $(this).text(i + 1);
+    });
+};
+
+var deleteButtonClick = function () {
+    var rows = $(".table-phone-book tr:has(td [type='checkbox']:checked)");
+    var messageString = "следующие контакты? :</br>";
+    $(".table-phone-book tr:has(td [type='checkbox']:checked) td:nth-child(2)").each(function () {
+        messageString += $(this).text() + "</br>";
+    });
+    if (rows.length === 0) {
+        rows = $(this).parents("tr");
+        messageString = "контакт: " + $(this).parents("tr").children("td:nth-child(2)").text() + " ?";
+    }
+
+    $.confirm({
+        title: "Подтверждение удаления",
+        content: "Вы действительно хотите удалить " + messageString,
+        confirmButton: "OK",
+        cancelButton: "Отмена",
+        confirm: function () {
+            rows.remove();
+            rowNumbered();
+        }
+    });
+};
